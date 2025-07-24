49d8a9162029689b40914671d4a257e6cc798427
SiekeMIS <lucasrosas.991402@gmail.com>
2025-07-24 10:29:17 -0400

    Ya hecho el models, el superuser

--- cachorros/models.py
+++ cachorros/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Cachorro(models.Model):
+    name_cachorro = models.CharField(max_length=200)
+    description_cachorro = models.TextField(blank=True)
+    adoptado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name_cachorro
